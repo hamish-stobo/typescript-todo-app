ecb5922e9fae2426ed023c8b43964203b5c95c7c
hamish-stobo <hh.stobo@gmail.com>
2021-05-29 18:22:38 +1200

    added persist on browser refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import AddTodo from './AddTodo'
 import Todo from './Todo'
@@ -12,10 +12,23 @@ function App(): JSX.Element {
     const removeTodo = (todo: string): void => {
         setTodos(todos.filter((todoEl) => todoEl !== todo))
     }
+    useEffect(() => {
+        const todosFromStorageString: string | null =
+            '' || localStorage.getItem('todos')
+        if (todosFromStorageString) {
+            setTodos([
+                ...todos,
+                ...todosFromStorageString.split('&1234|~xyz!_-'),
+            ])
+        }
+    }, [])
+    useEffect(() => {
+        localStorage.setItem('todos', todos.join('&1234|~xyz!_-'))
+    }, [todos])
     return (
         <>
             <div className="flex flex-col justify-center align-center mx-auto w-80 mt-20">
-                <h1 className="text-4xl my-4 text-center">Todo List App</h1>
+                <h1 className="text-4xl my-4 text-center">Todo List</h1>
                 <AddTodo AddFn={AddFn} />
                 {todos.map((todo: string) => (
                     <Todo key={uuidv4()} todo={todo} removeTodo={removeTodo} />
